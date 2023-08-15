8c26aa6d8de2d1f595af9868e765289596eccdf8
Vyaheslav <141916642+Vyahes1av@users.noreply.github.com>
2023-08-15 16:35:57 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+VANTA.CELLS({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  color1: 0x144475,
+  color2: 0x254b6b,
+  size: 1.00,
+  speed: 2.00
+})
